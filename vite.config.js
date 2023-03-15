9bc036efec96e5e3f22a810c4ebe9e3d6cd2a3c3
Ishaan-R1 <119463909+Ishaan-R1@users.noreply.github.com>
2023-03-15 17:14:34 +0000

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CW3_Part2_individual_complete_4/",
   plugins: [
     vue2(),
     legacy({
